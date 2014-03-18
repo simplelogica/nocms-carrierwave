2dd53cd2bd2fb0950285cdee02f92947720ebf99
David J. Brenes <david.brenes@simplelogica.net>
2014-03-18 15:41:42 +0100

    Creating the uploader for the attachment

--- app/models/no_cms/carrierwave/attachment.rb
+++ app/models/no_cms/carrierwave/attachment.rb
@@ -2,6 +2,10 @@ module NoCms::Carrierwave
   class Attachment < ActiveRecord::Base
     translates :attachment
 
+    delegate :attachment, :attachment=, to: :translation
+    Translation.mount_uploader :attachment, AttachmentUploader
+
     validates :attachment, presence: true
+
   end
 end
