44b0b4925ce6b4f8c96cf7c47975dbfe8a351d5f
David J. Brenes <david.brenes@simplelogica.net>
2014-03-18 15:43:50 +0100

    Testing the Carrierwave uploader

--- spec/models/no_cms/carrierwave/attachment_spec.rb
+++ spec/models/no_cms/carrierwave/attachment_spec.rb
@@ -4,4 +4,15 @@ describe NoCms::Carrierwave::Attachment do
 
   it_behaves_like "model with required attributes", :no_cms_carrierwave_attachment, [:attachment]
 
+  context 'when saving an attachment' do
+
+    let(:attachment) { NoCms::Carrierwave::Attachment.create(attributes_for(:no_cms_carrierwave_attachment)) }
+
+    subject { attachment }
+
+    it "should store it as a carrierwave attachment" do
+      expect(subject.attachment).to be_a NoCms::Carrierwave::AttachmentUploader
+    end
+
+  end
 end
