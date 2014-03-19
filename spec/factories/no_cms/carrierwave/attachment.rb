a503bf3665d022f055b19fc52bd69a44b6aa2910
David J. Brenes <david.brenes@simplelogica.net>
2014-03-19 18:21:26 +0100

    Creating versions of the attachment in the uploader
    Specs have been modified so we check that versions are created

--- spec/factories/no_cms/carrierwave/attachment.rb
+++ spec/factories/no_cms/carrierwave/attachment.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :no_cms_carrierwave_attachment, class: NoCms::Carrierwave::Attachment do
-    attachment { File.open(File.dirname(__FILE__) + '/../../../fixtures/attachments/attachment.png') }
+    attachment { File.open(__FILE__) } # The factory includes itself!!
+
+    factory :no_cms_carrierwave_image do
+      attachment { File.open(File.dirname(__FILE__) + '/../../../fixtures/attachments/attachment.png') }
+    end
   end
 end
--- spec/models/no_cms/carrierwave/attachment_spec.rb
+++ spec/models/no_cms/carrierwave/attachment_spec.rb
@@ -15,4 +15,33 @@ describe NoCms::Carrierwave::Attachment do
     end
 
   end
+
+  context 'when saving an image' do
+
+    let(:attachment) { NoCms::Carrierwave::Attachment.create(attributes_for(:no_cms_carrierwave_image)) }
+
+    subject { attachment }
+
+    it "should create versions" do
+      expect(subject.attachment.thumb.url).to_not eq subject.attachment.url
+      expect(subject.attachment.thumb.url).to_not be_nil
+    end
+
+    it "should not make up versions" do
+      expect{subject.attachment.thumb_fake}.to raise_error NoMethodError
+    end
+
+  end
+
+  context 'when saving an attachment (not image)' do
+
+    let(:attachment) { NoCms::Carrierwave::Attachment.create(attributes_for(:no_cms_carrierwave_attachment)) }
+
+    subject { attachment }
+
+    it "should not create versions" do
+      expect(subject.attachment.thumb.url).to be_nil
+    end
+
+  end
 end
