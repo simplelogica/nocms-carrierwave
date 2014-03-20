8305dd6e1bf44a21c68ad083a59fff9f8559cc50
David J. Brenes <david.brenes@simplelogica.net>
2014-03-20 10:12:59 +0100

    Name and description should be translated

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319173846) do
+ActiveRecord::Schema.define(version: 20140320090716) do
 
   create_table "no_cms_carrierwave_attachment_translations", force: true do |t|
     t.integer "no_cms_carrierwave_attachment_id"
     t.string  "locale"
     t.string  "attachment"
+    t.string  "name"
+    t.text    "description"
   end
 
   add_index "no_cms_carrierwave_attachment_translations", ["no_cms_carrierwave_attachment_id"], name: "no_cms_carr_img_on_translations"
@@ -24,8 +26,6 @@ ActiveRecord::Schema.define(version: 20140319173846) do
   create_table "no_cms_carrierwave_attachments", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.text     "description"
   end
 
 end
