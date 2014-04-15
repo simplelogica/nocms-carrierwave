67cb1ba607c053be3edee70eba4ff365e43106d4
David J. Brenes <david.brenes@simplelogica.net>
2014-04-15 17:59:47 +0200

    Overwriting changed? method of attachment::Translation class

--- app/models/no_cms/carrierwave/attachment.rb
+++ app/models/no_cms/carrierwave/attachment.rb
@@ -6,6 +6,15 @@ module NoCms::Carrierwave
     delegate :attachment, :attachment=, to: :translation
     Translation.mount_uploader :attachment, AttachmentUploader
 
+    class Translation
+
+      # We need to take into account the remove_attachment virtual attribute or else translations won't be saved
+      def changed?
+        super || !self.remove_attachment.blank?
+      end
+
+    end
+
     validates :attachment, presence: true
 
   end
