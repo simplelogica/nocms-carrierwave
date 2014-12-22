8325cc84d69b9e3e8db997e72b5666c0ce73f524
David J. Brenes <davidjbrenes@gmail.com>
2014-12-22 14:05:34 +0100

    Updating minimagick dependency for compatibility issues with debian testing
    This causes other gems to be updated (Including Rails to 4.2). Following the Rails update some warnings (mostly on configuration) have been fixed and errors_on method no longer exists.
    
    Pending migrations check is also giving problems, so we remove it.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,6 @@ require 'capybara/rspec'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("../support/**/*.rb")].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
 
   # Avoid repeating FactoryGirl
